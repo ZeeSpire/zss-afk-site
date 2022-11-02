00a7581cebf6e04c5f4690ef91e6239e538dbce2
Gabriel <ngvoicu@outlook.com>
2022-11-02 19:07:00 +0200

    org

--- git-push.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF 
-ECHO =============== Cleaning project ===============
-call ./clean.bat
-ECHO =============== Commit - Push to master ===============
-git add .
-git commit -m "Update"
-git push
-ECHO =============== Finished ===============
