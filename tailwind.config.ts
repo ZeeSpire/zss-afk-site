cd57548f9705f37e7b89747e01ff9c9c4a987a5b
Gabriel Voicu <gabriel.voicu@dipocket.org>
2024-09-26 23:12:22 +0300

    initial

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true, // Centers the container
+        padding: '1rem', // Add padding if needed
+        screens: {
+          sm: '100%',  // Full width for small screens
+          md: '640px', // Smaller width for medium screens
+          lg: '980px', // Custom smaller width for large screens
+          xl: '1100px', // Custom smaller width for extra-large screens
+        },
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        autumn: {
+          ...require("daisyui/src/theming/themes")["autumn"],
+          "background-color": "#ffffff",
+          "base-100": "#ffffff",
+        }
+      }
+    ]
+  }
+};
+export default config;
