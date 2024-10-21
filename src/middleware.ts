423558334430359d7e66c60880816bb1fa5e7287
Gabriel Voicu <gabriel.voicu@dipocket.org>
2024-10-21 20:22:52 +0300

    retry redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ const PUBLIC_FILE = /\.(.*)$/
 const slugs = getSlugs();
 
 function redirectbyCookie(req: NextRequest, lang?: string) {
-  console.log(">>> cookie is present")
   if (lang === 'ro') {
     console.log(">>> language is romanian")
 
@@ -58,28 +57,29 @@ export async function middleware(req: NextRequest) {
   // if(process.env.VERCEL) {
     if (req.nextUrl.pathname.startsWith('/_next') || req.nextUrl.pathname.includes('/api') || PUBLIC_FILE.test(req.nextUrl.pathname)) {
       return NextResponse.next(); // Skip middleware
-    }
-
-    console.log(">>> on VERCEL >>> starting redirects")
-  
-    const lang = req.cookies.get('AFK_LOCALE')?.value;
-  
-    if (lang) {
-      redirectbyCookie(req, lang);
     } else {
-    //   console.log(">>> cookie not present")
-    //   //check the country, set cookie and redirect to romanian
-    //   const country = req.headers.get('X-Vercel-IP-Country');
-    //   console.log(`>>> Country detected: ${country}`);
-
-    //   if (country === 'RO' || country === 'ro') {
-    //     console.log('>>> set ro cookie')
-    //     const response = NextResponse.next();
-    //     response.cookies.set('AFK_LOCALE', 'ro', { path: '/', maxAge: 60 * 60 * 24 * 365 }); // 1 year
-        
-    //     console.log('>>> redirecting to ro')
-    //     redirectbyCookie(req, 'ro');
-    //   }
-    // }
+      console.log(">>> on VERCEL >>> starting redirects")
+  
+      const lang = req.cookies.get('AFK_LOCALE')?.value;
+    
+      if (lang) {
+        redirectbyCookie(req, lang);
+      } else {
+      //   console.log(">>> cookie not present")
+      //   //check the country, set cookie and redirect to romanian
+      //   const country = req.headers.get('X-Vercel-IP-Country');
+      //   console.log(`>>> Country detected: ${country}`);
+  
+      //   if (country === 'RO' || country === 'ro') {
+      //     console.log('>>> set ro cookie')
+      //     const response = NextResponse.next();
+      //     response.cookies.set('AFK_LOCALE', 'ro', { path: '/', maxAge: 60 * 60 * 24 * 365 }); // 1 year
+          
+      //     console.log('>>> redirecting to ro')
+      //     redirectbyCookie(req, 'ro');
+      //   }
+      // }
+    }
+  
   }
 }
